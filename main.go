3a2a8b2a0d006d82012ed32888ef51909ac6615e
buglib <buglib@foxmail.com>
2023-03-14 11:50:41 +0800

    定义Task模型

--- main.go
+++ main.go
@@ -5,9 +5,22 @@ import (
 	"gorm.io/gorm"
 )
 
+// 定义任务状态
+const (
+	Done = iota
+	Todo
+)
+
+type Task struct {
+	gorm.Model
+	TaskInfo string
+	State    uint
+}
+
 func InitMysql() (db *gorm.DB, err error) {
 	dsn := "buglib:123456@tcp(localhost:3306)/todolist?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	db.AutoMigrate(&Task{})
 	return
 }
 
