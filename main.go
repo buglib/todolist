51b4ee73a51a1316e391b066c008667470b9b8ff
buglib <buglib@foxmail.com>
2023-03-14 12:11:30 +0800

    初始化Gin路由器

--- main.go
+++ main.go
@@ -1,9 +1,22 @@
 package main
 
 import (
+	"github.com/gin-gonic/gin"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
+
+var db *gorm.DB
+
+func main() {
+	err := initMysql()
+	if err != nil {
+		panic(err)
+	}
+
+	router := initRouter()
+	router.Run("0.0.0.0:8080")
+}
 
 // 定义任务状态
 const (
@@ -17,16 +30,47 @@ type Task struct {
 	State    uint
 }
 
-func InitMysql() (db *gorm.DB, err error) {
+func initMysql() (err error) {
 	dsn := "buglib:123456@tcp(localhost:3306)/todolist?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	db.AutoMigrate(&Task{})
 	return
 }
 
-func main() {
-	_, err := InitMysql()
-	if err != nil {
-		panic(err)
-	}
+func initRouter() *gin.Engine {
+	router := gin.Default()
+	router.LoadHTMLFiles("templates/index.html")
+	router.Static("/static", "static")
+	registerHandlers(router)
+	return router
+}
+
+func registerHandlers(router *gin.Engine) {
+	router.GET("/", getIndex)
+
+	v1 := router.Group("v1")
+	v1.POST("/todolist", postTodolist)
+	v1.GET("/todolist", getTodolist)
+	v1.PUT("/todolist/:id", putTodoItem)
+	v1.DELETE("/todolist/:id", deleteTodoItem)
+}
+
+func getIndex(ctx *gin.Context) {
+
+}
+
+func postTodolist(ctx *gin.Context) {
+
+}
+
+func getTodolist(ctx *gin.Context) {
+
+}
+
+func putTodoItem(ctx *gin.Context) {
+
+}
+
+func deleteTodoItem(ctx *gin.Context) {
+
 }
